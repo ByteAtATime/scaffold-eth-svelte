6d18e7ecb2f0a148af57d6462293e9f774e9e1ef
ByteAtATime <byteatatime@proton.me>
2023-09-26 18:16:10 +0000

    Initial Commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const buildNextEslintCommand = (filenames) =>
+  `pnpm next:lint --fix --file ${filenames
+    .map((f) => path.relative(path.join("packages", "nextjs"), f))
+    .join(" --file ")}`;
+
+const checkTypesNextCommand = () => "pnpm next:check-types";
+
+const buildHardhatEslintCommand = (filenames) =>
+  `pnpm hardhat:lint-staged --fix ${filenames
+    .map((f) => path.relative(path.join("packages", "hardhat"), f))
+    .join(" ")}`;
+
+module.exports = {
+  "packages/nextjs/**/*.{ts,tsx}": [
+    buildNextEslintCommand,
+    checkTypesNextCommand,
+  ],
+  "packages/hardhat/**/*.{ts,tsx}": [buildHardhatEslintCommand],
+};
