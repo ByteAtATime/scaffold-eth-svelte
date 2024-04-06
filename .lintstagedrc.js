43f19f5ad7ac1b7d4fea14855f9acfb4728eef84
ByteAtATime <ByteAtATime@proton.me>
2024-04-05 19:35:42 -0700

    Correct path in .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -13,7 +13,7 @@ const buildHardhatEslintCommand = (filenames) =>
     .join(" ")}`;
 
 module.exports = {
-  "packages/nextjs/**/*.{ts,tsx}": [
+  "packages/svelte/**/*.{ts,tsx}": [
     builtSvelteEslintCommand,
     checkTypesSvelteCommand,
   ],
