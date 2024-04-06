eddb1858c38681f7474e774922ba8d7ccfba7174
ByteAtATime <ByteAtATime@proton.me>
2024-04-05 21:39:10 -0700

    Correct class attribute in InheritanceTooltip

--- packages/svelte/src/routes/debug/_components/InheritanceTooltip.svelte
+++ packages/svelte/src/routes/debug/_components/InheritanceTooltip.svelte
@@ -6,6 +6,6 @@
 
 {#if inheritedFrom}
   <span class="tooltip tooltip-top tooltip-accent px-2 md:break-normal" data-tip={`Inherited from: ${inheritedFrom}`}>
-    <Icon src={InformationCircle} className="h-4 w-4" aria-hidden="true" />
+    <Icon src={InformationCircle} class="h-4 w-4" aria-hidden="true" />
   </span>
 {/if}
