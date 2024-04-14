0fe0a43c45a19669f950685836a2bde0599ed0cf
ByteAtATime <ByteAtATime@proton.me>
2024-04-14 12:26:46 -0700

    export ChooseTokenModal

--- packages/svelte/src/lib/components/scaffold-eth/index.ts
+++ packages/svelte/src/lib/components/scaffold-eth/index.ts
@@ -10,5 +10,6 @@ export type Token = {
 export { default as Address } from "./Address.svelte";
 export { default as Balance } from "./Balance.svelte";
 export { default as BlockieAvatar } from "./BlockieAvatar.svelte";
+export { default as ChooseTokenModal } from "./ChooseTokenModal.svelte";
 export { default as Faucet } from "./Faucet.svelte";
 export { default as FaucetButton } from "./FaucetButton.svelte";
